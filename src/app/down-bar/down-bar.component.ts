a07f4a394e4d1892d3c9d9c71497c23d6b9d03da
Gabriel Forny <82887059+gabrielforny@users.noreply.github.com>
2022-01-31 21:53:25 -0300

    Add files via upload

--- src/app/down-bar/down-bar.component.ts
+++ src/app/down-bar/down-bar.component.ts
@@ -1,1 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-down-bar',
+  templateUrl: './down-bar.component.html',
+  styleUrls: ['./down-bar.component.css']
+})
+
+export class DownBarComponent {
+}
 
